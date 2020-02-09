b35763dddafc452ac1e37bde23fc92782dd79d6c
Marcus Povey <marcus@marcus-povey.co.uk>
2020-02-09 11:24:27 +0000

    Edit course title stuff

--- templates/default/entity/Course/edit.tpl.php
+++ templates/default/entity/Course/edit.tpl.php
@@ -1,5 +1,15 @@
 <div class="course-form">
+    
+    <?php
+    if (empty($vars['object'])) {
+	?>
     <h2><?= \Idno\Core\Idno::site()->language()->_('Add a New Course'); ?></h2>
+    <?php
+    } else {
+	?>
+    <h2><?= \Idno\Core\Idno::site()->language()->_('Edit course %s', [$vars['object']->name]); ?></h2>
+    <?php
+    } ?>
     
     <form action="<?php echo $vars['object']->getURL() ?>" method="post" enctype="multipart/form-data">
 <?php
@@ -13,16 +23,133 @@
 	'values' => $vars['object']
     ])->draw('forms/input/form-list');
 ?>
+	<?php
+	    if (!empty($vars['object'])) {
+		?>
+	
+	<div class="row">
+	    <div class="col-sm-12 col-md-2">
+		<label>
+		    <?= \Idno\Core\Idno::site()->language()->_('Modules'); ?>
+		</label> 
+	    </div>
+	    <div class="col-sm-12 col-md-6">
+
+		<table id="modules">
+		    <thead>
+			<tr>
+			    <th>Module</th>
+			    <th>Action</th>
+			</tr>
+		    </thead>
+		    <tbody>
+		    <?php
+		    $modules = \IdnoPlugins\Courseware\Entities\Module::get(['course_id' => $vars['object']->getID()], [], PHP_INT_MAX); 
+
+		    if ($modules) {
+			foreach ($modules as $module) {
+			    ?>
+
+			    <tr>
+				<td><a href="<?= $module->getUrl(); ?>"><?= $module->name; ?></a></td>
+				<td style="width: 20%;">
+				    <?php if ($module->canEdit()) { ?>
+					<a href="<?= $module->getEditUrl() ?>" class="btn btn-primary"><?= \Idno\Core\Idno::site()->language()->_('Edit'); ?></a>
+					<?= $this->__([
+					    'url' => $module->getDeleteUrl(),
+					    'label' => \Idno\Core\Idno::site()->language()->_('Delete'),
+					    'class' => 'btn btn-danger',
+					    'confirm' => true,
+					    'confirm-text' => \Idno\Core\Idno::site()->language()->_('Are you sure you want to remove this course?')
+					])->draw('forms/link'); ?>
+				    <?php } ?>
+				</td>
+			    </tr>
+
+			    <?php
+			}
+		    }
+		    ?>
+		    </tbody>
+		</table>
+
+
+	    </div>
+	    <div class="col-sm-12 col-md-4">
+		<p class="config-desc"></p>
+	    </div>
+	</div>
+
+
+
+	<div class="row">
+	    <div class="col-sm-12 col-md-2">
+		<label>
+		    <?= \Idno\Core\Idno::site()->language()->_('Tasks and Events'); ?>
+		</label> 
+	    </div>
+	    <div class="col-sm-12 col-md-6">
+
+		<table id="schedule">
+		    <thead>
+			<tr>
+			    <th>Title</th>
+			    <th>Start date</th>
+			    <th>End date</th>
+			    <th>Action</th>
+			</tr>
+		    </thead>
+		    <tbody>
+		    <?php
+		    $events = \IdnoPlugins\Event\Event::get(['course_id' => $vars['object']->getID()], [], PHP_INT_MAX); 
+
+		    if ($events) {
+			foreach ($events as $event) {
+			    
+			    $starttime = strtotime($event->starttime);
+			    $endtime = strtotime($event->endtime);
+			    $timeformat = 'l, jS F Y h:i A';
+			    ?>
+
+			    <tr>
+				<td><a href="<?= $event->getUrl(); ?>"><?= $event->getTitle(); ?></a></td>
+				<td><time class="dt-start" datetime="<?php echo date('c', $starttime) ?>"><?php echo date($timeformat, $starttime)?></time></td>
+				<td><time class="dt-end" datetime="<?php echo date('c', $endtime) ?>"><?php echo date('h:i A', $endtime);?></time></td>
+				<td style="width: 20%;">
+				    <?php if ($module->canEdit()) { ?>
+					<a href="<?= $module->getEditUrl() ?>" class="btn btn-primary"><?= \Idno\Core\Idno::site()->language()->_('Edit'); ?></a>
+					<?= $this->__([
+					    'url' => $module->getDeleteUrl(),
+					    'label' => \Idno\Core\Idno::site()->language()->_('Delete'),
+					    'class' => 'btn btn-danger',
+					    'confirm' => true,
+					    'confirm-text' => \Idno\Core\Idno::site()->language()->_('Are you sure you want to remove this course?')
+					])->draw('forms/link'); ?>
+				    <?php } ?>
+				</td>
+			    </tr>
+
+			    <?php
+			}
+		    }
+		    ?>
+		    </tbody>
+		</table>
+
+
+	    </div>
+	    <div class="col-sm-12 col-md-4">
+		<p class="config-desc"></p>
+	    </div>
+	</div>	
+	<?php
+	    }
+	    ?>
+	
 	<?php echo $this->draw('content/extra'); ?>
 	<?php echo $this->draw('content/access'); ?>
 	
-	<div class="button-bar row">
-	    <?php echo \Idno\Core\Idno::site()->actions()->signForm('/course/edit') ?>
-	    <input type="button" class="btn btn-cancel" value="<?php echo \Idno\Core\Idno::site()->language()->_('Cancel'); ?>" onclick="hideContentCreateForm();"/>
-	    <input type="submit" class="btn btn-primary" value="<?php echo \Idno\Core\Idno::site()->language()->_('Save'); ?>"/>
-	    
-	    
-	</div>
+	
 	
 	<div class="row" style="margin-top:20px;">
 	    <?php
@@ -37,7 +164,33 @@
 	    }
 	    ?>
 	</div>
+	
+	
+	<div class="button-bar row" style="margin-top:20px;">
+	    <?php echo \Idno\Core\Idno::site()->actions()->signForm('/course/edit') ?>
+	    <input type="button" class="btn btn-cancel" value="<?php echo \Idno\Core\Idno::site()->language()->_('Cancel'); ?>" onclick="hideContentCreateForm();"/>
+	    <input type="submit" class="btn btn-primary" value="<?php echo \Idno\Core\Idno::site()->language()->_('Save'); ?>"/>
+	    
+	    
+	</div>
     </form>
 </div>
+<script>
+
+    $(document).ready(function() {
+	$('#modules').DataTable({
+	    "language": {
+		"emptyTable":     "No modules available"
+	    }
+	});
+	
+	$('#schedule').DataTable({
+	    "language": {
+		"emptyTable":     "No schedule events available"
+	    }
+	});
+    } );
+
+</script>
 
 <?php echo $this->draw('entity/edit/footer');?>
